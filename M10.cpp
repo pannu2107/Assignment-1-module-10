7bf3540fce403ef53e62dc6c4c217dcba404e0ea
Aman Pannu <amanpannu2107@gmail.com>
2024-07-13 20:42:47 +0530

    Add files via upload

---
+++ M10.cpp
@@ -0,0 +1,70 @@
+//Question 1
+// #include<iostream>
+// using namespace std;
+// void square(int a){
+//     for(int i=1;i<=a;i++){
+//         cout<<"square  of "<<i<<" is "<<i*i<<endl;
+//     }
+// }
+// int main(){
+//     square(6);
+// }
+
+//Question 2
+// #include<iostream>
+// using namespace std;
+// void area(int a){
+//     cout<<"Area of circle : "<<3.1415*a*a;
+// }
+// int main(){
+//     int z;
+//     cout<<"enter the radius : ";
+//     cin>>z;
+//     area(z);
+// }
+
+//Question 3
+// #include<iostream>
+// using namespace std;
+// int count;
+// void odd(int a,int b){
+//     for(int i=a+1;i<b;i++){
+//         if(i%2!=0) cout<<i<<" ";
+//     }
+// }
+// int main(){
+//     int a,b;
+//     cout<<"enter the two number : ";
+//     cin>>a>>b;
+//     odd(a,b);
+// }
+
+//Question 4
+// #include<iostream>
+// using namespace std;
+// int count;
+// void countsquare(int a){
+//     int count=0;
+//     while(a>0){
+//         a=a/10;
+//         count++;
+//     }
+//     cout<<count*count;
+// }
+// int main(){
+//     countsquare(47003972);
+// }
+
+//Question 5
+//infinite
+
+//Question 6
+// True
+// It depends that it is void function or int function for
+// void it is not necessary but int must return something
+
+//Question7
+// No
+// It will give error
+
+
